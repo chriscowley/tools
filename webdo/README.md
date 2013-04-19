07a8e5c0cb68ffe71722d69a7dfd48d187698966
Chris Cowley <chris@chriscowley.me.uk>
2013-04-19 21:00:18 +0100

    Added Webdo listener that listens for Github Web URL hook scripts and executes a shell script specified by a CLI argument

--- webdo/README.md
+++ webdo/README.md
@@ -1,1 +1,9 @@
-Server to respond to Github Web URL hooks
+Server to respond to Github Web URL hooks and execute a something.
+
+usage: listen.py [-h] -s SCRIPT
+
+I run it my web server user (nginx) so that it can write into that directory. The easiest way to do this is to add a line to /etc/rc.local that look something like:
+
+```
+su -s /bin/bash - nginx -c "/usr/local/bin/listen.py --script /usr/local/bin/cv-update"
+```
--- webdo/listen.py
+++ webdo/listen.py
@@ -2,15 +2,19 @@
 
 from flask import Flask, request
 import subprocess
+import argparse
 
+parser = argparse.ArgumentParser()
+parser.add_argument('-s', '--script', required=True)
+args = parser.parse_args()
+script =  args.script
+print script
 app = Flask(__name__)
 
 @app.route('/',methods=['POST'])
 def foo():
-    process = subprocess.Popen('/usr/local/bin/cv-update', shell=True, stdout=subprocess.PIPE)
-
+    process = subprocess.Popen(script, shell=True, stdout=subprocess.PIPE)
     process.wait()
-    print process.returncode
     return "OK"
 
 if __name__ == '__main__':
